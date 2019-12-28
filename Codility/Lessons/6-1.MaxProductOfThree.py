48bfb45a14137be612bffb58c4eed4b487cbe3e7
ii200400 <ii200400@naver.com>
2019-12-28 20:52:15 +0900

    레슨 6-1 해결 정확성이 많이 어려웠다.

--- Codility/Lessons/6-1.MaxProductOfThree.py
+++ Codility/Lessons/6-1.MaxProductOfThree.py
@@ -2,13 +2,15 @@ import heapq, itertools
 
 def solution(A):
     m_heap = []
+    re_m_heap = []
     heap = []
     
     for num in A:
-        if num > 0:
+        if num >= 0:
             heapq.heappush(heap, -num)
         else:
             heapq.heappush(m_heap, num)
+            heapq.heappush(re_m_heap, -num)
     
     nums = []
     for _ in range(3):
@@ -17,11 +19,18 @@ def solution(A):
         else:
             break
         
-    for _ in range(2):
-        if len(m_heap) > 0:
-            nums.append(heapq.heappop(m_heap))
-        else:
-            break
+    if len(nums) == 0:
+        for _ in range(3):
+            if len(re_m_heap) > 0:
+                nums.append(-heapq.heappop(re_m_heap))
+            else:
+                break
+    else:      
+        for _ in range(3):
+            if len(m_heap) > 0:
+                nums.append(heapq.heappop(m_heap))
+            else:
+                break
     
     answer = -1000000001
     combin = list(itertools.combinations(nums, 3))
@@ -31,3 +40,5 @@ def solution(A):
             answer = mul
             
     return answer
+
+# 효율성 보다는 정확성이 어려웠던 문제였다. 다음에 다시 풀어봐야겠다.
