1cd155e6acefc9fe4aba1fd52995feeb8b030727
ii200400 <ii200400@naver.com>
2020-01-03 21:18:37 +0900

    7-2 정확성을 통과하는 코드

--- Codility/Lessons/7-2.Fish.py
+++ Codility/Lessons/7-2.Fish.py
@@ -1,3 +1,6 @@
+# 정확성 검사에서 한 테스트케이스에서 timeout 된 코드
+# 스택은 사용 안하고  전체 리스트에서 하나씩 del하여 만들었다.
+
 def solution(A, B):
     C = [(B[i], A[i]) for i in range(len(A))]
     i = 0
@@ -15,4 +18,27 @@ def solution(A, B):
         else:
             i += 1
             
+    return len(C)
+
+# 정확성 검사를 통과하는 코드
+# 스택을 사용하였다.
+
+def solution(A, B):
+    C = []
+    for i in range(len(A)):
+        #print(C)
+        if len(C) == 0 or B[i] == 1:
+            C.append((B[i], A[i]))
+        elif B[i] == 0:
+            while True:
+                l = len(C) - 1
+                if len(C) == 0 or C[l][0] == 0:
+                    C.append((B[i], A[i]))
+                    break
+                elif C[l][1] > A[i]:
+                    break
+                else:
+                    C.pop()
+    
+    #print(C)
     return len(C)
