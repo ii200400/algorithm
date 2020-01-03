b072b41ef124883cb3390eebcc9b02b9584b6716
ii200400 <ii200400@naver.com>
2020-01-03 14:32:19 +0900

    레슨 7-1 정확성 문제 해결중

---
+++ Codility/Lessons/7-1.Brackets.py
@@ -0,0 +1,19 @@
+def solution(S):
+    if len(S) == 0:
+        return 0
+    
+    stack = [S[0]]
+    for char in S[1:]:
+        if char in ['(', '[', '{']:
+            stack.append(char)
+        elif len(stack) > 0:
+            p = stack[len(stack)-1]
+            if ((char == ')' and p == '(') or (char == ']' and p == '[') or (char == '}' and p == '{')):
+                stack.pop()
+        else :
+            return 0
+    
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
