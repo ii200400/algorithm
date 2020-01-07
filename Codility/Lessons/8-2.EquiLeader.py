dde90767c3d7b67c89fb0d2c2e1a232515afb5c3
ii200400 <ii200400@naver.com>
2020-01-08 06:05:30 +0900

    8-2에서 쓰이는 counter공부중..

--- Codility/Lessons/8-2.EquiLeader.py
+++ Codility/Lessons/8-2.EquiLeader.py
@@ -1,15 +1,23 @@
 from collections import Counter
 
 def solution(A):
-    count = 0
+    answer = 0
     left_dic = Counter([])
     right_dic = Counter(A)
     
-    A.pop()
     cnt = 0
+    leng = len(A)
+    A.pop()
     for num in A:
         left_dic[num] += 1
         right_dic[num] -= 1
         cnt += 1
         
-        if left_dic
+        left_lead = list(left_dic.items())[0]
+        right_lead = list(right_dic.items())[0]
+        # print(cnt, left_lead, right_lead)
+        if left_lead[0] == right_lead[0] and left_lead[1] > cnt/2 and right_lead[1] > (leng-cnt)/2:
+            # print("Y")
+            answer += 1
+    
+    return answer
