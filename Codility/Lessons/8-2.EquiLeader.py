6e9be106c6a9ea856a383ab61f7768b1903cde59
ii200400 <ii200400@naver.com>
2020-01-08 22:53:42 +0900

    8-2 효율성 부분 패스;;

--- Codility/Lessons/8-2.EquiLeader.py
+++ Codility/Lessons/8-2.EquiLeader.py
@@ -15,9 +15,48 @@ def solution(A):
         
         left_lead = max(list(left_dic.items()), key = lambda x: x[1])
         right_lead = max(list(right_dic.items()), key = lambda x: x[1])
-        # print(cnt, left_dic, right_dic, left_lead, right_lead)
+        print(cnt, left_dic, right_dic, left_lead, right_lead)
         if left_lead[0] == right_lead[0] and left_lead[1] > cnt/2 and right_lead[1] > (leng-cnt)/2:
-            # print("Y")
+            print("Y")
             answer += 1
     
     return answer
+
+# 이것도 timeout 된다...
+def solution2(A):
+    answer = 0
+    left_dic = {}
+    right_dic = {}
+    for num in A:
+        right_dic[num] = right_dic.get(num, 0) + 1
+    
+    cnt = 0
+    leng = len(A)
+    A.pop()
+    
+    left_lead, right_lead = None,  None
+    for num in A:
+        if left_dic.get(num, 0) == 0:
+            left_dic[num] = 1
+            
+            if left_lead == None:
+                left_lead = (num, 1)
+        else:
+            left_dic[num] += 1
+            left_lead = max(list(left_dic.items()), key = lambda x: x[1])
+            
+        right_dic[num] -= 1
+        if right_lead == None or right_lead[0] == num:
+            right_lead = max(list(right_dic.items()), key = lambda x: x[1])
+            
+        cnt += 1
+        
+        print(cnt, left_dic, right_dic, left_lead, right_lead)
+        if left_lead[0] == right_lead[0] and left_lead[1] > cnt/2 and right_lead[1] > (leng-cnt)/2:
+            print("Y")
+            answer += 1
+    
+    return answer
+
+
+solution2([i for i in range(100000)])
