bb7e62f719e7d9c695617d6f721ca08eb452a512
ii200400 <ii200400@naver.com>
2020-01-19 00:38:15 +0900

    데모 결과 기록 및 10-1 문제 해결

---
+++ Codility/Lessons/10-1.MinPerimeterRectangle.py
@@ -0,0 +1,10 @@
+#에라테노스의 체였나 그것을 사용하면 더 빠를 것 같기는 한데 쓰지는 않고
+#그냥 제곱근의 속도(O(sqrt(N)))로만 해결하도록 하였다.
+
+import math
+
+def solution(N):
+    sqrt = int(math.sqrt(N))
+    for num in range(sqrt,0,-1):
+        if N % num == 0:
+            return 2 * (num + N//num)
--- Codility/Lessons/2-1.OddOccurrencesInArray.py
+++ Codility/Lessons/2-1.OddOccurrencesInArray.py
@@ -7,3 +7,15 @@ def solution(A):
     for key, val in dic.items():
         if val % 2 == 1: 
             return key
+
+def solution(A):
+    dic = dict()
+    
+    for num in A:
+        if dic.get(num, 0) == 0:
+            dic[num] = 1
+        else:
+            del dic[num]
+    
+    for key in dic.keys():
+        return key
