056736384686310a6000cc24b5d533f8bc6ae0f5
ii200400 <ii200400@naver.com>
2020-01-20 19:46:47 +0900

    레슨 10-3 해결

--- Codility/Lessons/10-3.Peaks.py
+++ Codility/Lessons/10-3.Peaks.py
@@ -1,11 +1,29 @@
 import math
 
 def solution(A):
-    peak_list = [0 for i in range(len(A)//2)]
+    l = len(A)
+    
+    def check(leng):
+        temp = -1
+        
+        for peak in peak_list:
+            if temp == peak//leng:
+                continue
+            elif temp+1 == peak//leng:
+                temp += 1
+            else:
+                return False
+        
+        if temp == math.ceil(l/leng)-1:
+            return True
+        else:
+            return False
+        
+    peak_list = [0 for i in range(l//2)]
     peak_index = 0
     isup = False
     
-    for index in range(1,len(A)):
+    for index in range(1,l):
         if A[index-1] == A[index]:
             isup = False
         elif  A[index-1] < A[index]:
@@ -19,25 +37,27 @@ def solution(A):
         return 0
     
     peak_list = peak_list[:peak_index]
-    # 탐색한 길이 중 답이 될 수 없는 가장 긴 길이
-    mi = (len(A)-1)//peak_index
-    # 탐색한 길이 중 답이 될 수 있는 가장 짧은 길이, 변수 mi보다는 크다.
-    ma = len(A)
-    while mi+1 != ma:
-        mid = (mi+ma)//2
-        temp = -1
-        for num in peak_list:
-            if temp == num//mid:
-                continue
-            elif temp+1 == num//mid:
-                temp += 1
+    start = int(math.sqrt(l))
+    isup = None
+    for num in range(start,0,-1):
+        if l % num == 0:
+            if check(num):
+                isup = False
             else:
-                break
+                isup = True
             
-        # print(mi, ma, temp)
-        if temp == math.ceil(len(A)/mid)-1:
-            ma = mid
-        else:
-            mi = mid
-            
-    return math.ceil(len(A)/ma)
+            start = num
+            break
+    
+    answer = 0
+    for num in range(start,0,-1):
+        if l % num == 0:
+            if isup == True and check(l//num):
+                return num
+            elif isup == False:
+                if check(num):
+                    answer = num
+                else:
+                    break
+    
+    return l//answer
