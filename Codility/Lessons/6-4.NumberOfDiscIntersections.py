d32d21cbab2482bb22b5eacfb2ac7617e1745b9d
ii200400 <ii200400@naver.com>
2020-04-07 16:38:37 +0900

    레슨 6-4 코드 추가

--- Codility/Lessons/6-4.NumberOfDiscIntersections.py
+++ Codility/Lessons/6-4.NumberOfDiscIntersections.py
@@ -1,3 +1,4 @@
+# heap을 활용해서 해결한 알고리즘이다.
 import heapq
 
 def solution(A):
@@ -22,3 +23,21 @@ def solution(A):
         return -1
     else:
         return answer
+
+# 위와 같은 알고리즘을 heap대신 sort함수로 구현한 것
+def solution(A):
+    stack = [(idx // 2 - A[idx // 2], 1) if idx % 2 == 0
+             else (idx // 2 + A[idx // 2], 0)
+             for idx in range(len(A) * 2)]
+
+    answer = 0
+    count = 0
+    stack.sort(key=lambda x: (x[0], -x[1]))
+    for num, typ in stack:
+        if typ == 1:
+            answer += count
+            count += 1
+        else:
+            count -= 1
+
+    return answer if answer <= 10000000 else -1
