e54c2e0eb8b291429bc10084ce875ebb2b3a5c32
ii200400 <ii200400@naver.com>
2021-06-10 19:58:51 +0900

    6월 10일 코딩스터디 lcs 코딩 도중

---
+++ Baekjoon/LCS.py
@@ -0,0 +1,11 @@
+# 문제 링크 : https://www.acmicpc.net/problem/9251
+# 제출 공유 링크 : 
+# 도움받은 블로그 링크 : https://velog.io/@emplam27/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B7%B8%EB%A6%BC%EC%9C%BC%EB%A1%9C-%EC%95%8C%EC%95%84%EB%B3%B4%EB%8A%94-LCS-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Longest-Common-Substring%EC%99%80-Longest-Common-Subsequence
+
+str1 = input()
+str2 = input()
+
+
+for i in range(len(str1)):
+    for j in range(len(str2)):
+
---
+++ Baekjoon/포도주 시식.py
@@ -0,0 +1,21 @@
+# 문제 링크 : https://www.acmicpc.net/problem/2156
+# 제출 공유 링크 : http://boj.kr/2281f47c72c94f85bf7e1a0e95da7f3d
+# 도움받은 질문 링크 : https://www.acmicpc.net/board/view/64747
+
+# import sys
+# input = sys.stdin.readline
+
+# 포도주 양 리스트
+n = int(input())
+amounts = [int(input()) for i in range(n)]
+max_amt = [0 for i in range(n)]
+
+max_amt[0] = amounts[0]
+
+if n > 1: max_amt[1] = amounts[0] + amounts[1]
+if n > 2: max_amt[2] = amounts[2] + max(amounts[0], amounts[1])
+
+for i in range(3, n):
+    max_amt[i] = amounts[i] + max(max_amt[i-2], amounts[i-1] + max_amt[i-3], amounts[i-1] + max_amt[i-4])
+
+print(max(max_amt))
