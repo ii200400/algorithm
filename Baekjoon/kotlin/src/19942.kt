0374103045ee5d0901d94cc1ed01b3dbba428fb3
ii200400 <ii200400@naver.com>
2021-11-24 00:02:37 +0900

    2021년 11월 16일 화요일 코딩스터디 백준의 19942 다이어트 해설 추가 및 사전순 비교 방법 추가

--- Baekjoon/kotlin/src/19942.kt
+++ Baekjoon/kotlin/src/19942.kt
@@ -1,10 +1,20 @@
 // 문제 링크 : https://www.acmicpc.net/problem/19942
 // 제출 공유 링크 : http://boj.kr/ec74792adf384caca0058506397dd0df
 
+// 완전탐색을 이용해서 음식을 선택하는 모든 경우를 살펴보기 위해 재귀 함수(searchAllFoods)를 사용한다.
+// 사전순으로 더 빠른 것을 선택해야 하므로 compareOrder라는 함수를 만들어 선택한 음식들의 순서를 비교하였다.
+
+// foodsNumber : 음식 수
+// nutritionStandard : 최소 영양소 리스트
+// foods : 각 음식들의 영양소 배열
+// searchFoods : 탐색중인 음식 세트
+// selectedFoods : 현재까지 탐색한 음식 세트 중 가장 가격이 싸고 사전순으로 빠른 것
+
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main19942() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    // 입력값
     val foodsNumber = readLine().toInt()
     val nutritionStandard = readLine().split(" ").map{ it.toInt() }
     val foods = Array(foodsNumber){ readLine().split(" ").map { it.toInt() }.toIntArray() }
@@ -12,6 +22,8 @@ fun main19942() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val searchFoods = BooleanArray(foods.size){ false }
     var selectedFoods = BooleanArray(foods.size){ false }
     var price = 8000
+
+    // 선택한 음식들의 영양분과 가격의 총합 반환
     fun getNutritionSum(selectedFoods: BooleanArray): IntArray {
         val nutritionSum = intArrayOf(0,0,0,0,0)
         for (idx in selectedFoods.indices){
@@ -26,23 +38,37 @@ fun main19942() = with(BufferedReader(InputStreamReader(System.`in`))) {
 
         return nutritionSum
     }
+    // 현재 탐색중인 음식들이 사전순으로 더 빠르다면 true, 그렇지 않다면 false를 반환한다.
     fun compareOrder(): Boolean{
-        var selectedFoodsMark = -1
-        var searchFoodsMark = -1
-        for (idx in selectedFoods.indices){
-            if (selectedFoods[idx] == searchFoods[idx] && selectedFoodsMark == -1 && searchFoodsMark == -1) continue
-            if (selectedFoods[idx] && selectedFoodsMark == -1) selectedFoodsMark = idx
-            if (searchFoods[idx] && searchFoodsMark == -1) searchFoodsMark = idx
-        }
-//        println("$selectedFoodsMark and $searchFoodsMark")
+//        첫번째 방법 (사전순으로 더 빠른지 두 음식 세트를 하나하나 비교하여 확인한다.)
+//        var selectedFoodsMark = -1
+//        var searchFoodsMark = -1
+//        for (idx in selectedFoods.indices){
+//            if (selectedFoods[idx] == searchFoods[idx] && selectedFoodsMark == -1 && searchFoodsMark == -1) continue
+//            if (selectedFoods[idx] && selectedFoodsMark == -1) selectedFoodsMark = idx
+//            if (searchFoods[idx] && searchFoodsMark == -1) searchFoodsMark = idx
+//        }
+////        println("$selectedFoodsMark and $searchFoodsMark")
+//
+//        if (selectedFoodsMark > searchFoodsMark) return true
+//        return false
 
-        if (selectedFoodsMark > searchFoodsMark) return true
+//        두번째 방법 (string 대소비교는 사전순인 것을 활용하여, 음식들을 string형으로 변환하여 바로 대소 비교를 해준다.)
+        var selectedFoodsString = ""
+        var searchFoodsString = ""
+        for (idx in selectedFoods.indices){
+            if (selectedFoods[idx]) selectedFoodsString += (idx+1).toString()
+            if (searchFoods[idx]) searchFoodsString += (idx+1).toString()
+        }
+        if (selectedFoodsString > searchFoodsString) return true
         return false
     }
-    fun dfs(idx: Int){
+    // 완전탐색
+    fun searchAllFoods(idx: Int){
         if (idx == foods.size){
             val compareTo = getNutritionSum(searchFoods)
 //            println(searchFoods.contentToString())
+            // 최소 영양소 기준을 넘으면서 가격이 더 싸거나, 가격은 같으면서 사전순으로 더 빠르면 음식 세트를 기억한다.
             if (compareTo[0] >= nutritionStandard[0]
                 && compareTo[1] >= nutritionStandard[1]
                 && compareTo[2] >= nutritionStandard[2]
@@ -57,14 +83,16 @@ fun main19942() = with(BufferedReader(InputStreamReader(System.`in`))) {
             return
         }
 
-        dfs(idx+1)
+        searchAllFoods(idx+1)
         searchFoods[idx] = true
-        dfs(idx+1)
+        searchAllFoods(idx+1)
         searchFoods[idx] = false
     }
 
-    dfs(0)
+    // 완전 탐색 시작
+    searchAllFoods(0)
 
+    // 조건에 맞는 답이 없는 경우
     if (price == 8000){
         println(-1)
         return
@@ -74,4 +102,8 @@ fun main19942() = with(BufferedReader(InputStreamReader(System.`in`))) {
     for (idx in selectedFoods.indices){
         if (selectedFoods[idx]) print("${idx+1} ")
     }
+}
+
+fun main() {
+    main19942()
 }
