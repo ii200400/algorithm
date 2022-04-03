5b7b433ec0d0146e016a324f218f152d92d526db
임영선 <ii200400@naver.com>
2022-04-03 19:52:02 +0900

    구미 알고리즘 스터디 문제 2개 (경사로, 암호만들기) 주석 뒤늦게 추가

--- Baekjoon/kotlin/src/com/baekjoon/problem/java14890/Main.java
+++ Baekjoon/kotlin/src/com/baekjoon/problem/java14890/Main.java
@@ -19,7 +19,7 @@ public class Main {
         int n = sc.nextInt();   // 지도 크기
         int l = sc.nextInt();   // 경사로 길이
         int[][] map = new int[n+2][n+2];    // 지도 배열 (예외처리를 위해 크기 +2)
-        boolean[][] isRamp = new boolean[n+2][n+2];
+        boolean[][] isRamp = new boolean[n+2][n+2]; // 경사로 설치 여부
 
         // 지도 초기화 (주변을 -1로 초기화 예외처리를 좀 쉽게 하려고)
         Arrays.fill(map[0], -1);
@@ -34,31 +34,43 @@ public class Main {
 
         int answer = 0;
 
-        // 가로 경사로를 탐색해본다.
+        // 가로 경사로를 
         loadR:for (int i = 1; i<=n; i++) {
+            // 한 칸씩 탐색해보는데
             int j;
             for (j = 1; j<n; j++) {
+                
+                // 다음 칸과 현재 칸의 높이 차이가 1이라면(현재 칸이 더 높음)
                 int hDiff = map[i][j] - map[i][j + 1];
                 if (hDiff == 1) {
                     int height = map[i][j + 1];
 
+                    // 탐색 진행방향으로 l만큼을 탐색하면서
                     for (int k = 1; k <= l; k++) {
+                        // 각 칸의 높이가 같지 않거나 이미 경사로가 놓여져 있다면 다음 줄을 살펴보러가고
                         if (map[i][j+k] != height || isRamp[i][j+k]){
                             continue loadR;
                         }
+                        // 그렇지 않으면 경사로를 잘 설치하고 지나간다.......?
+                        // 이거.. 백준 테스트 케이스가.. 잘못되어있나본데..? 내가 오해하고 있나?
                         isRamp[i][j+k] = true;
                     }
 
+                    // 다음 칸과 현재 칸의 높이 차이가 1이라면(현재 칸이 더 낮음)
                 }else if (hDiff == -1){
                     int height = map[i][j];
 
+                    // 탐색 진행방향의 반대방향으로 l만큼을 탐색하면서
                     for (int k = 0; k < l; k++) {
+                        // 각 칸의 높이가 같지 않거나 이미 경사로가 놓여져 있다면 다음 줄을 살펴보러가고
                         if (map[i][j-k] != height || isRamp[i][j-k]){
                             continue loadR;
                         }
+                        // 그렇지 않으면 경사로를 잘 설치하고 지나간다
                         isRamp[i][j-k] = true;
                     }
                 }else if(hDiff != 0){
+                    // 만약 현재 칸과의 차이가 1이상 차이나면 다음 줄을 탐색한다.
                     continue loadR;
                 }
             }
@@ -67,7 +79,8 @@ public class Main {
             answer++;
         }
 
-        // 세로 경사로를 탐색해본다.
+        // 세로 경사로도 가로 경사로 탐색과 같이 살펴본다.
+        // 주석은 생략한다.
         isRamp = new boolean[n+2][n+2];
         loadC:for (int i = 1; i<=n; i++) {
             int j;
--- Baekjoon/kotlin/src/com/baekjoon/problem/java1759/Main.java
+++ Baekjoon/kotlin/src/com/baekjoon/problem/java1759/Main.java
@@ -19,27 +19,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        l = sc.nextInt();
-        c = sc.nextInt();
-        chars = new char[c];
+        l = sc.nextInt();   // 선택할 문자 수
+        c = sc.nextInt();   // 문자 종류 수
+        chars = new char[c];    // 문자 배열
 
+        // 문자 배열 초기화
         for (int i = 0; i<c; i++){
             chars[i] = sc.next().charAt(0);
         }
-        Arrays.sort(chars);
-        vowels = new HashSet<>();
+        Arrays.sort(chars); // 사전순으로 출력하기 위해 정렬
+        vowels = new HashSet<>();   // 모음들, 그냥 배열쓰고 isContain해도 되는데 기분삼아 쓴 해시셋
+        // 해시셋 초기화
         for (char ch: new char[]{'a','e', 'i', 'o', 'u'}){
             vowels.add(ch);
         }
 
+        // 선택된 문자들
         selected = new char[l];
         dfs(0, 0, 0);
     }
 
     // 조합 (선택한 수의 개수, 조합을 이어나갈 인덱스, 모음 개수)
     static void dfs(int cnt, int idx, int v){
+        // 문자를 l개 선택했다면
         if (cnt == l){
+            // 모음과 자음 수가 조건을 충족하는지 확인하고
             if (v > 0 && l-v > 1){
+                // 조건에 충족되면 출ㄺ한다.
                 for (int i = 0; i<l; i++){
                     System.out.print(selected[i]);
                 }
@@ -48,12 +54,13 @@ public class Main {
             return;
         }
 
+        // 문자 선택!
         for (int i = idx; i<c; i++){
             selected[cnt] = chars[i];
 
-            if (vowels.contains(chars[i])){
+            if (vowels.contains(chars[i])){ // 선택한 문자가 모음일 경우
                 dfs(cnt+1, i+1, v+1);
-            }else{
+            }else{  // 선택한 문자가 자음일 경우
                 dfs(cnt+1, i+1, v);
             }
         }
